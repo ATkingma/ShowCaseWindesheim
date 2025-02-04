8136c263ca7b06bedec39320e54cd4328e654286
ATkingma <60423088+ATkingma@users.noreply.github.com>
2025-02-05 00:30:34 +0100

    darkmode

---
+++ webdev-showcase-studentversie-main/UC1-Profielpagina/Showcase-Profielpagina/wwwroot/js/darkmode.js
@@ -0,0 +1,24 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    const body = document.body;
+
+    if (localStorage.getItem("theme") === "light") {
+        body.classList.add("light-mode");
+        darkModeToggle.textContent = "Dark Mode";
+    } else {
+        body.classList.remove("light-mode");
+        darkModeToggle.textContent = "Light Mode";
+    }
+
+    darkModeToggle.addEventListener("click", function () {
+        body.classList.toggle("light-mode");
+
+        if (body.classList.contains("light-mode")) {
+            localStorage.setItem("theme", "light");
+            darkModeToggle.textContent = "Dark Mode";
+        } else {
+            localStorage.setItem("theme", "dark");
+            darkModeToggle.textContent = "Light Mode";
+        }
+    });
+});
