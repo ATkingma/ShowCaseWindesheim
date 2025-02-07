c002eb2160e7018c491b0fb5449706237e1b6f6e
ATkingma <60423088+ATkingma@users.noreply.github.com>
2025-02-07 17:22:03 +0100

    updated for security

--- webdev-showcase-studentversie-main/UC1-Profielpagina/Showcase-Profielpagina/Program.cs
+++ webdev-showcase-studentversie-main/UC1-Profielpagina/Showcase-Profielpagina/Program.cs
@@ -1,23 +1,49 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.AddServerHeader = false;
+});
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("X-Frame-Options", "DENY");
+    context.Response.Headers.Add("Content-Security-Policy", "frame-ancestors 'self';");
+    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+    await next();
+});
+
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = ctx =>
+    {
+        var fileName = ctx.File.Name;
+        if (fileName.EndsWith("Thumbs.db", StringComparison.OrdinalIgnoreCase) ||
+            fileName.EndsWith(".DS_Store", StringComparison.OrdinalIgnoreCase) ||
+            fileName.StartsWith(".git", StringComparison.OrdinalIgnoreCase) ||
+            fileName.StartsWith(".svn", StringComparison.OrdinalIgnoreCase))
+        {
+            ctx.Context.Response.StatusCode = 403;
+            ctx.Context.Response.Body = Stream.Null;
+        }
+    }
+});
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
